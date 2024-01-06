4fd38d611506b03319d36ee48928c1546a9f823f
khanhtn <khanhtnfptdn@gmail.com>
2024-01-07 03:09:02 +0700

    clear code

--- src/components/result-dialog/ResultDialog.jsx
+++ src/components/result-dialog/ResultDialog.jsx
@@ -1,5 +1,5 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
-import React, { useState } from "react";
+import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function ResultDialog({ isOpen, handleRefreshClip, result, onClose }) {
