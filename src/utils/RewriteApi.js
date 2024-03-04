c22a57d679534a221038f0547710511aecb6904b
khanhtn <khanhtnfptdn@gmail.com>
2024-03-04 13:36:01 +0700

    fix backend api

--- src/utils/RewriteApi.js
+++ src/utils/RewriteApi.js
@@ -1,7 +1,7 @@
-const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:5001";
+const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:5001/";
 
 const ApiUrl = (api) => {
-  return `${backendUrl}/api/v1${api}`;
+  return `${backendUrl}api/v1${api}`;
 };
 
 export default ApiUrl;
