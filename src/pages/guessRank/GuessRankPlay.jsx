af49309a58429fe00b9ef3ac9130bd3a54870ff8
khanhtn <nhat-khanht@system-exe.com.vn>
2024-05-23 10:44:07 +0700

    test response

--- src/pages/guessRank/GuessRankPlay.jsx
+++ src/pages/guessRank/GuessRankPlay.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import {
+  Box,
   Button,
+  CardMedia,
   Dialog,
   DialogActions,
   DialogContent,
@@ -9,6 +11,12 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
 } from "@mui/material";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import DoDisturbOffIcon from "@mui/icons-material/DoDisturbOff";
@@ -47,6 +55,106 @@ function GuessRankPlay({ game, open, onClose, ranks }) {
             <ListItemText primary="Đoán sai sẽ bị trừ 3 điểm." />
           </ListItem>
         </List>
+        <Typography variant="h6">Ví dụ:</Typography>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Rank chính xác</TableCell>
+              <TableCell align="center">Rank bạn chọn</TableCell>
+              <TableCell align="center">Điểm nhận được</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <TableRow>
+              <TableCell align="center">
+                <Box className="flex justify-center flex-col items-center">
+                  <CardMedia
+                    component="img"
+                    sx={{ width: "30px" }}
+                    image={ranks[4]?.image}
+                    alt={ranks[4]?.rankName}
+                  />
+                  <Typography variant="body2">{ranks[4]?.rankName}</Typography>
+                </Box>
+              </TableCell>
+              <TableCell align="center">
+                <Box className="flex justify-center flex-col items-center">
+                  <CardMedia
+                    component="img"
+                    sx={{ width: "30px" }}
+                    image={ranks[5]?.image}
+                    alt={ranks[5]?.rankName}
+                  />
+                  <Typography variant="body2">{ranks[5]?.rankName}</Typography>
+                </Box>
+              </TableCell>
+              <TableCell align="center">
+                <Typography variant="body1" sx={{ color: "green" }}>
+                  + 2
+                </Typography>
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell align="center">
+                <Box className="flex justify-center flex-col items-center">
+                  <CardMedia
+                    component="img"
+                    sx={{ width: "30px" }}
+                    image={ranks[4]?.image}
+                    alt={ranks[4]?.rankName}
+                  />
+                  <Typography variant="body2">{ranks[4]?.rankName}</Typography>
+                </Box>
+              </TableCell>
+              <TableCell align="center">
+                <Box className="flex justify-center flex-col items-center">
+                  <CardMedia
+                    component="img"
+                    sx={{ width: "30px" }}
+                    image={ranks[4]?.image}
+                    alt={ranks[4]?.rankName}
+                  />
+                  <Typography variant="body2">{ranks[4]?.rankName}</Typography>
+                </Box>
+              </TableCell>
+              <TableCell align="center">
+                <Typography variant="body1" sx={{ color: "green" }}>
+                  + 5
+                </Typography>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell align="center">
+                <Box className="flex justify-center flex-col items-center">
+                  <CardMedia
+                    component="img"
+                    sx={{ width: "30px" }}
+                    image={ranks[4]?.image}
+                    alt={ranks[4]?.rankName}
+                  />
+                  <Typography variant="body">{ranks[4]?.rankName}</Typography>
+                </Box>
+              </TableCell>
+              <TableCell align="center">
+                <Box className="flex justify-center flex-col items-center">
+                  <CardMedia
+                    component="img"
+                    sx={{ width: "30px" }}
+                    image={ranks[2]?.image}
+                    alt={ranks[2]?.rankName}
+                  />
+                  <Typography variant="body2">{ranks[2]?.rankName}</Typography>
+                </Box>
+              </TableCell>
+              <TableCell align="center">
+                <Typography variant="body1" sx={{ color: "red" }}>
+                  - 3
+                </Typography>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       </DialogContent>
       <DialogActions>
         <Button onClick={onClose} variant="contained" color="success">
